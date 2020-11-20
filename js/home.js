ecdad27aabf93ca4386877957e07ff6fd2034029
jindalakhil <akhiljindal222@gmail.com>
2020-11-20 23:58:14 +0530

    [akhil]added: employee payroll data from local storage

--- js/home.js
+++ js/home.js
@@ -1,53 +1,34 @@
+let empPayrollList;
+
 window.addEventListener("DOMContentLoaded", (event) => {
+    empPayrollList=getEmployeePayrollDataFromStoarge();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
  });
-
+ const getEmployeePayrollDataFromStoarge = () => {
+    return localStorage.getItem("EmployeePayrollList") ? JSON.parse(localStorage.getItem("EmployeePayrollList")) : [];
+};
  const createInnerHtml = () => {
-
+ if(empPayrollList.length==0)return;
    const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>"
    let innerHtml = `${headerHtml}`;
-   let empPayrollList = createEmployeePayrollJSON();
    for(const empPayrollData of empPayrollList){
    innerHtml = `${headerHtml}
                 <tr>
-                    <td><img class="profile" src="${empPayrollData._picture}"></td>
+                    <td><img class="profile" src="${empPayrollData._profilePic}"></td>
                     <td>${empPayrollData._name}</td>
                     <td>${empPayrollData._gender}</td>
                     <td>${getDeptHtml(empPayrollData._department)}</td>
                     <td>${empPayrollData._salary}</td>
                     <td>${empPayrollData._startDate}</td>
-                    <td><img id="1" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
-                        <img id="1" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
+                    <td><img id="${empPayrollData._id}" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
+                        <img id="${empPayrollData._id}" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
                     </td>
                 </tr>
     `;
     document.querySelector("#display").innerHTML = innerHtml;
  }
- const createEmployeePayrollJSON = () => {
-    let empPayrollListLocal = [
-        {
-        _name: "Narayan Mahadevan",
-        _gender: "male",
-        _department: ['Engineering','Finance'],
-        _salary: "500000",
-        _startDate: "29 Oct 2019",
-        _note: "",
-        _id: new Date().getTime(),
-        _picture: "../assets/profile-images/Ellipse -2.png"
-        },
-    {
-     _name: "Amarpa Shashanka Keerthi Kumar",
-     _gender: "female",
-     _department: ['Sales'],
-     _salary: "400000",
-     _startDate: "29 Oct 2019",
-     _note: "",
-     _id: new Date().getTime() + 1,
-     _picture: "../assets/profile-images/Ellipse -1.png"
-    }
-    ];
-    return empPayrollListLocal;
- }
+}
 
  const getDeptHtml = (deptList) => {
     let deptHtml = "";
@@ -56,3 +37,4 @@ window.addEventListener("DOMContentLoaded", (event) => {
     }
     return deptHtml;
 }
+ 
